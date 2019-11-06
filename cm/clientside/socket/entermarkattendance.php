396276c05deb86da5f8057491ceb5fb8ce3c3bf7
MukeshMonk <mukesh@monkdesk.in>
2019-11-06 15:33:09 +0530

    Enter mark attendance "STATUS ISSUE SOLVED"

--- cm/clientside/socket/entermarkattendance.php
+++ cm/clientside/socket/entermarkattendance.php
@@ -10,8 +10,9 @@
 	$sort_field_value=$app->getPostVar('sort_field_value');
     $markattandence_id=$app->getPostVar('markattandence_id');
     $class_id=$app->getPostVar('class_id');
-    $add_date=$app->getPostVar('add_date');
-
+	$add_date=$app->getPostVar('add_date');
+	
+	
 	if($sort_field!="" && $sort_field_value!="")
 	{
 		$ord_cond=" order by ". $sort_field." ".$sort_field_value;
@@ -56,10 +57,15 @@
 		$id=$row['id'];
 		// $name=$row['name'];
 		$id_enc=$app->cmx->encrypt($row['id'],ency_key);
-    	$student_atten_check=$app->cmx->check_student_attendence($row['id'],date('Y-m-d',$add_date));	
+		$student_atten_check=$app->cmx->check_student_attendence($id,date('Y-m-d',$add_date));
+		
+		$df=$app->cmx->get_status_for_attandenace($markattandence_id);
+
+
+		
         $check_leave=count($student_atten_check);
 
-
+		//echo "<pre>"; print_r($student_atten_check);  exit;
 		$html.='<input type="hidden" name="student_id1[]" value="'.$row['id'].'"/>';
 		$html.='<input type="hidden" name="course1[]" value="'.$row['cm_course'].'"/>';
 		$html.='<input type="hidden" name="sem1[]" value="'. $row['sem'].'"/>';
@@ -74,10 +80,12 @@
 
         $status_array1=array(""=>"Select Status","Present"=>"Present","Absent"=>"Absent","Official Leave"=>"Official Leave","Medical Leave"=>"Medical Leave");
         $status_array=array(""=>"Select Status","Present"=>"Present","Absent"=>"Absent");
-					
+ 
+		//echo "<pre>"; print_r($selected_vallue); exit;
 					if($check_leave>0)
 					{						
 						$selected_vallue=$student_atten_check[0]['default_status']; 
+						
 						$maxatten_id=$student_atten_check[0]['id']; 
 						
 						if($selected_vallue=='Official Leave' || $selected_vallue=='Medical Leave')
@@ -92,19 +100,21 @@
 					}
 					else
 					{
-						$selected_vallue=$default_status;$disabled="";					
+						$selected_vallue=$df;$disabled="";	
+									
 						$remark="";
 						$maxatten_id="";
 					}
 					
 					
 					if($disabled!="")
-					{
+					{	
 						$html.=$app->cmx->buildInsideTag("select", array("class"=>"select2 w150","id"=>"default_status",  "name"=>"default_status11[]","values"=>$status_array1,"selected"=>$selected_vallue,"disabled"=>"disabled"),"");
 						$app->cmx->buildInsideTag("input",array("type"=>"hidden","name"=>"default_status1[]","id"=>"default_status_".$maxatten_id,"value"=>"NA"),"");
 					}
 					else
 					{
+						
 						$html.=$app->cmx->buildInsideTag("select", array("class"=>"select2  w150","id"=>"default_status",  "name"=>"default_status1[]","values"=>$status_array,"selected"=>$selected_vallue),"");
 					}
 					
